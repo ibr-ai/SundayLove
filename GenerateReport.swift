88e3ccda1fa7663c07e1191f14e5baee5319e722
ibr-ai <ibrahima4@lasalle.edu>
2024-08-26 16:28:41 -0400

    Fixed structure of build

---
+++ GenerateReport.swift
@@ -0,0 +1,14 @@
+//
+//  GenerateReport.swift
+//  SundayLove
+//
+//  Created by Toaster Strudle, Ja! on 8/26/24.
+//
+
+import Foundation
+
+func generateReport() {
+    
+    print("Not in production yet!")
+    
+}
